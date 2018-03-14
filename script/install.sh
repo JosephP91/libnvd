ad81379529544d18a88000780ca3c445e15c7e26
Giuseppe Persico <giuseppe.persico91@gmail.com>
2018-03-14 11:56:58 +0100

    Add files via upload
    Fixes

--- script/install.sh
+++ script/install.sh
@@ -11,16 +11,16 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 sudo apt-get update
 sudo apt-get -y install cmake g++ pkg-config mongodb-org php-mongodb
 
-# Install mongoc driver (1.8.2 because it's the recommended version for mongodb 3.2)
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.8.2/mongo-c-driver-1.8.2.tar.gz
-tar -zxf mongo-c-driver-1.8.2.tar.gz
-rm ./mongo-c-driver-1.8.2.tar.gz
-cd mongo-c-driver-1.8.2
+# Install mongoc driver (1.9.2 because it's the recommended version for mongodb 3.2)
+wget https://github.com/mongodb/mongo-c-driver/releases/download/1.9.2/mongo-c-driver-1.9.2.tar.gz
+tar -zxf mongo-c-driver-1.9.2.tar.gz
+rm ./mongo-c-driver-1.9.2.tar.gz
+cd mongo-c-driver-1.9.2
 ./configure --disable-automatic-init-and-cleanup
 make
 sudo make install
 cd ../
-rm -rf ./mongo-c-driver-1.8.2
+rm -rf ./mongo-c-driver-1.9.2
 
 # Install last stable release of mongocxx driver
 git clone https://github.com/mongodb/mongo-cxx-driver.git --branch releases/stable --depth 1
