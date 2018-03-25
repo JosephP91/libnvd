09e7122b6c19a5df5d2d6585a06f48c49fbbfc3d
Giuseppe Persico <giuseppe.persico91@gmail.com>
2018-03-25 19:36:05 +0200

    Add mongodb 3.2 apt key file.

--- script/install.sh
+++ script/install.sh
@@ -13,6 +13,10 @@ esac
 
 # If we are on Linux, install mongodb and ensure all packages are installed.
 if [ "$machine" == "Linux" ] ;then
+    # Apt key for mongodb 3.2
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
     # Install various packages.
     sudo apt-get update
     sudo apt-get -y install cmake g++ pkg-config mongodb-org php-mongodb lynx unzip
